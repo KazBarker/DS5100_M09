3ec40203e76e117da6cd5fd62987d350fa788fba
KazBarker <kbarker747@gmail.com>
2023-10-24 20:20:17 -0400

    Added files for class

---
+++ M09_challenge/challenge_module.py
@@ -0,0 +1,5 @@
+def print_challenge():
+    '''
+    Prints "Hello world!"
+    '''
+    print('Hello world!')
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='M09_challenge',
+    version='1.0.0',
+    description='Package for inclass challenge.',
+    url='https://github.com/KazBarker/DS5100_M09/tree/main',
+    author='Kaz Barker',
+    author_email='nrg6cb@virginia.edu',
+    license='MIT',
+    packages=['M09_challenge']
+)
